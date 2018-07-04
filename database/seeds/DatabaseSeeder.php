2e9ebaffcc3bf303fe3589134a787bd298d719fe
Diego Giovani Meza Ramirez <diego.meza@gsscogdl.com>
2018-07-03 21:52:05 -0500

    Agregamos roles y permisos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \YaroslavMolchan\Rbac\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,37 @@ class DatabaseSeeder extends Seeder
     	foreach (App\User::all() as $user) {
     		$user->materials()->attach(rand(1, App\Material::count()), ['notes' => 'ta bueno']);
     	}
+        if (!Role::count()) {
+            $adminRole = Role::create([
+                'name' => 'Administrator',
+                'slug' => 'admin'
+            ]);
+            $lenderRole = Role::create([
+                'name' => 'Lender',
+                'slug' => 'lender'
+            ]);
+            //Probably will change to debtor
+            $debtorRole = Role::create([
+                'name' => 'Debtor',
+                'slug' => 'debtor'
+            ]);
+            $adminUser = factory(App\User::class)->create([
+                'email' => 'admin@admin.admin'
+
+            ]);
+            $lenderUser = factory(App\User::class)->create([
+                'email' => 'lender@lender.lender'
+
+            ]);
+            $debtorUser = factory(App\User::class)->create([
+                'email' => 'debtor@debtor.debtor'
+
+            ]);
+            $adminUser->attachRole($adminRole);
+            //or you can insert only id
+            $lenderUser->attachRole($lenderRole);
+            $debtorUser->attachRole($debtorRole);
+        }
         // $this->call(UsersTableSeeder::class);
     }
 }
