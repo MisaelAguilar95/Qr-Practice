76f7f8645c39ae5766a7e5e719c97ae5d2417e95
Diego Meza <diego.dgmr.dm@gmail.com>
2018-07-03 21:50:48 -0500

    Add frontend structure

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,11 @@
+  // import env from '@/env'
+import '@/bootstrap'
+import '@/Components/Prototype.js'
+import '@/Components/Directives.js'
+import '@/Components/Globals'
+import store from '@/Store'
+import router from '@/Routes/Router'
+import Vue from 'vue'
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -5,18 +13,64 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// const app = new Vue
+export default new Vue({
 
-const app = new Vue({
-    el: '#app'
-});
+  /**
+   * The Dom element where the
+   * root instance wil be render.
+   * ------------------------------
+   * @member {string} el
+   */
+  el: '#app',
+
+  /**
+   * The VueRouter Instance.
+   * ------------------------------
+   * @member {Object} router
+   */
+  router,
+
+  /**
+   * The Vuex Instance.
+   * ------------------------------
+   * @member {Object} store
+   */
+  store,
+
+  /**
+   * The main instance reactive
+   * properties of the component.
+   * ------------------------------
+   * @member {Object}}
+   */
+  data: {
+    //
+  },
+
+  /**
+   * The main instance methods of
+   * the component.
+   * ------------------------------
+   * @member {Object} methods
+   */
+  methods: {
+    //
+  },
+
+  /**
+   * The mounted hook life-cycle of
+   * the component instance.
+   * ------------------------------
+   * @member {Function}
+   */
+  mounted () {
+    //
+  }
+})
